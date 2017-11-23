a3e74d6713be248e1654dfc995db4462e6fc7491
xiaoqiangcs <liuwenqiangcs@gmail.com>
2017-11-23 21:00:00 +0800

    fix bug

--- Test.py
+++ Test.py
@@ -1,14 +1,8 @@
 # -*- coding:utf-8 -*-
 """
 @author:Wenqiang Liu
-@file:Test.py
-@time:2017/11/918:53
+@file:test.py
+@time:2017/11/2317:02
 """
-import tensorflow as tf
-a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
-# Creates a session with log_device_placement set to True.
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-# Runs the op.
-print(sess.run(c))
+n=[1,2,3,4]
+print(sum(n))
