b16785488f55c28987daf39650c3d184fc790512
xiaoqiangcs <liuwenqiangcs@gmail.com>
2017-12-14 17:16:49 +0800

    fix bug

--- KGGAN/TransE.py
+++ KGGAN/TransE.py
@@ -33,9 +33,9 @@ class TransE(object):
 	# 定义生成器
 	def generator(self,noise_Z):
 		# 根据噪声Z进行多层感知机操作，生成num_negative个负样例
-		G_W1 = tf.Variable(self.variable_init([self.num_negative * self.noise_dim, self.num_negative * 100]),dtype=tf.float32)
-		G_b1 = tf.Variable(tf.zeros(shape=[self.num_negative * 100]), dtype=tf.float32)
-		G_W2 = tf.Variable(self.variable_init([self.num_negative * 100, self.num_negative * self.embedding_dimension]),dtype=tf.float32)
+		G_W1 = tf.Variable(self.variable_init([self.num_negative * self.noise_dim, self.num_negative * 64]),dtype=tf.float32)
+		G_b1 = tf.Variable(tf.zeros(shape=[self.num_negative * 64]), dtype=tf.float32)
+		G_W2 = tf.Variable(self.variable_init([self.num_negative * 64, self.num_negative * self.embedding_dimension]),dtype=tf.float32)
 		G_b2 = tf.Variable(tf.zeros(shape=[self.num_negative * self.embedding_dimension]), dtype=tf.float32)
 		theta_G = [G_W1, G_W2, G_b1, G_b2]  # 梯度下降的参数范围
 		# 第一层先计算 y=z*G_W1+G-b1,然后投入激活函数计算G_h1=ReLU（y）,G_h1 为第二次层神经网络的输出激活值
@@ -55,28 +55,44 @@ class TransE(object):
 			D_prob =tf.nn.sigmoid(-dissimilarity)
 		D_log_prob = tf.log(D_prob+eps)
 		# 输出G_log_prob是构建损失函数
-		return D_log_prob
+		D_log_loss = tf.reduce_sum(D_log_prob,axis=0)
+		return D_log_loss
 
 	def get_negative_embedding(self, G_prob, num_entity):
 		# 根据生成的实体，从图中获取离该节点最近的点的距离，并把该样例作为负样例get embedding from the graph
 		batch_negative = []  # 定义一个存储负样例的矩阵，矩阵大小为(batch_size *(num_negative*3))
-		generator_samples= tf.reshape(G_prob,shape=[self.batch_size,self.num_negative,self.embedding_dimension])
+		generator_samples = tf.reshape(G_prob,shape=[self.batch_size,self.num_negative,self.embedding_dimension])
 		loss_sum = 0.0
 		with tf.variable_scope('embedding', reuse=True):
 			embedding_entity = tf.get_variable(name='entity')
-		embedding_entity_extend = tf.tile(embedding_entity,[self.num_negative,1])
+		num_index = self.num_negative // 20
+		embedding_entity_extend = tf.tile(embedding_entity,[num_index,1])
 		for batch_index in range(self.batch_size):
-			generator_index_samples=generator_samples[batch_index,:]
-			generator_index_samples_extend = tf.reshape(tf.tile(generator_index_samples,[1,num_entity]),shape=[self.batch_size*num_entity,self.embedding_dimension])
-			if self.dissimilarity == 'L2':
-				dissimilarity = tf.sqrt(tf.reduce_sum(tf.square(generator_index_samples_extend - embedding_entity_extend), axis=1))
-			else:  # default: L1
-				dissimilarity = tf.reduce_sum(tf.abs(generator_index_samples_extend - embedding_entity_extend), axis=1)
-			dissimilarity_extend = tf.reshape(dissimilarity,shape=[self.batch_size,num_entity])
-			batch_negative.append(tf.argmin(dissimilarity_extend,axis=1))
-			loss_sum += tf.reduce_sum(tf.reduce_max(dissimilarity_extend,axis=1))
+			generator_index_samples = generator_samples[batch_index, :]
+			for nagative_index in range(20):
+				generator_nagative_index = generator_index_samples[nagative_index*num_index:(nagative_index+1)*num_index]
+				generator_nagative_samples_extend = tf.reshape(tf.tile(generator_nagative_index,[1,num_entity]),[num_entity*num_index,self.embedding_dimension])
+				if self.dissimilarity == 'L2':
+					dissimilarity = tf.sqrt(tf.reduce_sum(tf.square(generator_nagative_samples_extend - embedding_entity_extend), axis=1))
+				else:  # default: L1
+					dissimilarity = tf.reduce_sum(tf.abs(generator_nagative_samples_extend - embedding_entity_extend), axis=1)
+				dissimilarity_extend = tf.reshape(dissimilarity, shape=[num_index, num_entity])
+				batch_negative.append(tf.argmin(dissimilarity_extend,axis=1))
+				loss_sum += tf.reduce_sum(tf.reduce_min(dissimilarity_extend,axis=1))
+		batch_negative = tf.reshape(batch_negative,shape=[self.batch_size,self.num_negative])
 		return batch_negative,loss_sum
-	def get_negative_sampling(self, G_prob, num_entity):
+
+	def get_negative_sampling(self, batch_negative, id_triplets_positive):
+		# 根据生成的候选负样实体h'或者t',构建候选负样例(h',r,t)或者(h',r,t')
+		batch_negative_extend= tf.reshape(batch_negative,[-1,1])
+		triplets_positive = tf.reshape(tf.tile(id_triplets_positive,[1,self.num_negative]),[-1,3])
+		index_positive= tf.Variable([1,2])
+		batch_negative_sample = tf.concat([batch_negative_extend,tf.gather(triplets_positive,index_positive,axis=1)],axis=1)
+		g_loss_fake = self.discriminator(batch_negative_sample,postive=True)
+		batch_negative_sample = tf.reshape(batch_negative_sample,[self.batch_size,-1])
+		return batch_negative_sample, g_loss_fake
+
+	def thumbel_softmax(self, G_prob, num_entity):
 		# 根据生成的实体，从图中获取离该节点最近的点的距离，并把该样例作为负样例get embedding from the graph
 		batch_negative = []  # 定义一个存储负样例的矩阵，矩阵大小为(batch_size *(num_negative*3))
 		generator_samples= tf.reshape(G_prob,shape=[self.batch_size,self.num_negative,self.embedding_dimension])
